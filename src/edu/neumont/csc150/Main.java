780eeafe18a365b951d7dcb252b529a59ef8fedf
Mathew Sjoquist <msjoquist@student.neumont.edu>
2022-02-28 16:44:08 -0700

    Gui Test

--- src/edu/neumont/csc150/Main.java
+++ src/edu/neumont/csc150/Main.java
@@ -1,4 +1,9 @@
 package edu.neumont.csc150;
 
+import edu.neumont.csc150.controller.PuzzleController;
+
 public class Main {
+	public static void main(String[] args) {
+		new PuzzleController().run();
+	}
 }
