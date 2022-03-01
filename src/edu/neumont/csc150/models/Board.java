1ed93c88b44ae3e9dc30a10e0de91466bfed4a25
Emile Randolph <rrandolph@student.neumont.edu>
2022-03-01 13:29:57 -0700

    the shuffle meathod does not work

--- src/edu/neumont/csc150/models/Board.java
+++ src/edu/neumont/csc150/models/Board.java
@@ -1,94 +1,107 @@
 package edu.neumont.csc150.models;
 
 public class Board {
-	Square[][] board;
-	Difficulty difficulty;
-	int size;
+    Square[][] board;
+    Difficulty difficulty;
+    int size;
 
-	public Board(Difficulty difficulty) {
-		setDifficulty(difficulty);
-	}
+    public Board(Difficulty difficulty) {
+        setDifficulty(difficulty);
+    }
 
-	public void setUpGame(){
-		setUpBoard();
-		shuffle();
-	}
+    public void setUpGame() {
+        setUpBoard();
+        shuffle();
+    }
 
-	public void setUpBoard(){
-		board = new Square[size][size];
-		int num = 1;
-		for (int row = 0; row < size; row++) {
-			for (int col = 0; col < size; col++) {
-				board[row][col] = new Square();
-				board[row][col].setValue(num);
-				num++;
-			}
-		}
-		board[size-1][size-1].setValue(0);
-	}
+    public void setUpBoard() {
+        board = new Square[size][size];
+        int num = 1;
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                board[row][col] = new Square();
+                board[row][col].setValue(num);
+                num++;
+            }
+        }
+        board[size - 1][size - 1].setValue(0);
+    }
 
-	public void shuffle(){
-		for (int i = 0; i < 50; i++) {
-			for (int row = 0; row < size; row++) {
-				for (int col = 0; col < size; col++) {
-					int holdNum;
-					if(row!=0) {
-						if (board[row - 1][col].getValue() == 0) {
-							board[row - 1][col].setValue(board[row][col].getValue());
-							board[row][col].setValue(0);
-						}
-					}
-					if(row!=2) {
-						if (board[row + 1][col].getValue() == 0) {
-							board[row + 1][col].setValue(board[row][col].getValue());
-							board[row][col].setValue(0);
-						}
-					}
-					if(col!=0) {
-						if (board[row][col - 1].getValue() == 0) {
-							board[row][col - 1].setValue(board[row][col].getValue());
-							board[row][col].setValue(0);
-						}
-					}
-					if(col!=2) {
-						if (board[row][col + 1].getValue() == 0) {
-							board[row][col + 1].setValue(board[row][col].getValue());
-							board[row][col].setValue(0);
-						}
-					}
-				}
-			}
-		}
-	}
+    public void shuffle() {
+        for (int i = 0; i < 50; i++) {
+            for (int row = 0; row < size; row++) {
+                for (int col = 0; col < size; col++) {
+                    int holdLastRow = 0;
+                    int holdLastCol = 0;
+                    soutBoard();
+                    if (holdLastCol == col && holdLastRow == row) {
+                        break;
+                    } else {
+                        if (col != 0 && board[row][col - 1].getValue() == 0) {
+                            board[row][col - 1].setValue(board[row][col].getValue());
+                            board[row][col].setValue(0);
+                            holdLastRow = row;
+                            holdLastCol = col - 1;
+                            break;
+                        } else if (col != 2 && board[row][col + 1].getValue() == 0) {
+                            board[row][col + 1].setValue(board[row][col].getValue());
+                            board[row][col].setValue(0);
+                            holdLastRow = row;
+                            holdLastCol = col + 1;
+                            break;
+                        } else if (row != 0 && board[row - 1][col].getValue() == 0) {
+                            board[row - 1][col].setValue(board[row][col].getValue());
+                            board[row][col].setValue(0);
+                            holdLastRow = row - 1;
+                            holdLastCol = col;
+                            break;
+                        } else if (row != 2 && board[row + 1][col].getValue() == 0) {
+                            board[row + 1][col].setValue(board[row][col].getValue());
+                            board[row][col].setValue(0);
+                            holdLastRow = row + 1;
+                            holdLastCol = col;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-	public Difficulty getDifficulty() {
-		return difficulty;
-	}
 
-	public void setDifficulty(Difficulty difficulty) {
-		this.difficulty = difficulty;
-		switch (difficulty){
-			case easy -> setSize(3);
-			case medium -> setSize(5);
-			case hard -> setSize(7);
-		}
-	}
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
 
-	public int getSize() {
-		return size;
-	}
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+        switch (difficulty) {
+            case easy -> setSize(3);
+            case medium -> setSize(5);
+            case hard -> setSize(7);
+        }
+    }
 
-	private void setSize(int size) {
-		this.size = size;
-	}
+    public int getSize() {
+        return size;
+    }
 
-	public void checkingToSeeIfThingsWork(){
-		setUpGame();
-		for (int row = 0; row < size; row++) {
-			for (int col = 0; col < size; col++) {
-				System.out.print(" "+board[row][col].getValue());
-			}
-			System.out.println();
-		}
-	}
+    private void setSize(int size) {
+        this.size = size;
+    }
+
+    public void checkingToSeeIfThingsWork() {
+        setUpGame();
+        soutBoard();
+    }
+
+    public void soutBoard() {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                System.out.print(" " + board[row][col].getValue());
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
--- src/edu/neumont/csc150/models/Square.java
+++ src/edu/neumont/csc150/models/Square.java
@@ -1,31 +1,31 @@
 package edu.neumont.csc150.models;
 
 public class Square {
-	private int row;
-	private int column;
-	private int value;
+    private int row;
+    private int column;
+    private int value;
 
-	public int getRow() {
-		return row;
-	}
+    public int getRow() {
+        return row;
+    }
 
-	public void setRow(int row) {
-		this.row = row;
-	}
+    public void setRow(int row) {
+        this.row = row;
+    }
 
-	public int getColumn() {
-		return column;
-	}
+    public int getColumn() {
+        return column;
+    }
 
-	public void setColumn(int column) {
-		this.column = column;
-	}
+    public void setColumn(int column) {
+        this.column = column;
+    }
 
-	public int getValue() {
-		return value;
-	}
+    public int getValue() {
+        return value;
+    }
 
-	public void setValue(int value) {
-		this.value = value;
-	}
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
